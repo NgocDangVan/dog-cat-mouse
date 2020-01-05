617198d7a08471c3b229d4d3e7423e58f86d8a08
NgocDangVan <ngocdangvan98@gmail.com>
2020-01-05 14:28:32 +0700

    let dog eat cat

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var Dog = require('./Dog');
+var Cat = require('./cat');
 var newDog = new Dog('Tom');
-newDog.sayHi();
+// newDog.sayHi();
+
+var Tom = new Cat();
+newDog.eat(Tom);
+console.log(newDog);
